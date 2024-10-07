e620adeab29ce775af306fcc29002be24869fa26
Dan Greenberg <dan.greenberg01@gmail.com>
2024-10-07 14:26:05 -0700

    minor fixes to model fits

--- stan models/code/model_fitting.R
+++ stan models/code/model_fitting.R
@@ -197,7 +197,7 @@ write.csv(bh_chm_eca_st$summary(),'./stan models/outs/summary/bh_chm_eca_st.csv'
 bh_chm_eca_st$save_object('./stan models/outs/fits/bh_chm_eca_st.RDS')
 
 post_bh_chm_eca_st=bh_chm_eca_st$draws(variables=c('b_for','b_for_cu','b_for_rv','alpha_j','Rk','sigma'),format='draws_matrix')
-write.csv(post_bh_chm_eca_st,here('stan models','outs','posterior','bh_chm_eca.csv'))
+write.csv(post_bh_chm_eca_st,here('stan models','outs','posterior','bh_chm_eca_st.csv'))
 
 
 ric_chm_eca_st <- mric_st$sample(data=dl_chm_eca,
