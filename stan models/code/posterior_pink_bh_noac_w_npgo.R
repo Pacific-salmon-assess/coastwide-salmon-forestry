1546ac2315153984e07ef26d021b4e22f6a6ead0
maria-kuruvilla <maria75799@gmail.com>
2024-12-03 18:11:09 -0800

    changed load to readrds

--- stan models/code/posterior_pink_bh_noac_w_npgo.R
+++ stan models/code/posterior_pink_bh_noac_w_npgo.R
@@ -24,7 +24,7 @@ source(here('stan models','code','funcs.R'))
 
 
 if(Sys.info()[7] == "mariakur") {
-  bh_pk_eca <- load(here('stan models','outs','fits','bh_pk_eca_noac_w_npgo_trial.RDS'))
+  bh_pk_eca <- readRDS(here('stan models','outs','fits','bh_pk_eca_noac_w_npgo_trial.RDS'))
   post_bh_pk_eca=bh_pk_eca$draws(variables=c('b_for','b_for_cu','b_for_rv',
                                              'b_npgo','b_npgo_cu','b_npgo_rv',
                                              'alpha_j','Rk','sigma_for_cu','sigma_for_rv'),format='draws_matrix')
@@ -32,7 +32,7 @@ if(Sys.info()[7] == "mariakur") {
   
   #repeat for cpd
   
-  bh_pk_cpd <- load(here('stan models','outs','fits','bh_pk_cpd_noac_w_npgo_trial.RDS'))
+  bh_pk_cpd <- readRDS(here('stan models','outs','fits','bh_pk_cpd_noac_w_npgo_trial.RDS'))
   post_bh_pk_cpd=bh_pk_cpd$draws(variables=c('b_for','b_for_cu','b_for_rv',
                                              'b_npgo','b_npgo_cu','b_npgo_rv',
                                              'alpha_j','Rk','sigma_for_cu','sigma_for_rv'),format='draws_matrix')
@@ -41,14 +41,14 @@ if(Sys.info()[7] == "mariakur") {
   
   
 }else{
-  bh_pk_eca <- load(here('stan models','outs','fits','bh_pk_eca_noac_w_npgo.RDS'))
+  bh_pk_eca <- readRDS(here('stan models','outs','fits','bh_pk_eca_noac_w_npgo.RDS'))
   post_bh_pk_eca=bh_pk_eca$draws(variables=c('b_for','b_for_cu','b_for_rv',
                                              'b_npgo','b_npgo_cu','b_npgo_rv',
                                              'alpha_j','Rk','sigma_for_cu','sigma_for_rv'),format='draws_matrix')
   write.csv(post_bh_pk_eca,here('stan models','outs','posterior','bh_pk_eca_noac_w_npgo.csv'))
   
   
-  bh_pk_cpd <- load(here('stan models','outs','fits','bh_pk_cpd_noac_w_npgo.RDS'))
+  bh_pk_cpd <- readRDS(here('stan models','outs','fits','bh_pk_cpd_noac_w_npgo.RDS'))
   post_bh_pk_cpd=bh_pk_cpd$draws(variables=c('b_for','b_for_cu','b_for_rv',
                                              'b_npgo','b_npgo_cu','b_npgo_rv',
                                              'alpha_j','Rk','sigma_for_cu','sigma_for_rv'),format='draws_matrix')
